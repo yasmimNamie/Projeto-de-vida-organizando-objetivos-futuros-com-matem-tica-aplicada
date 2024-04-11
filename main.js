594caad0bf029434cfd0dfcc94741b34473e6435
yasmim :) <108752556+yasmimNamie@users.noreply.github.com>
2024-04-11 16:23:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -56,3 +56,21 @@ function comecaCronometro(){
 }
 
 comecaCronometro();
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = tempoFinal / 1000;
+  return segundos;
+}
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+    return dias;
+  }
